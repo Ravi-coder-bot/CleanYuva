8d74f6ea07302323d0a652ed7fc9e64b1cd3b56f
Geetika-Behl <142812594+Geetika-Behl@users.noreply.github.com>
2025-02-26 18:39:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB (Local Machine)
-const localMongoURI = 'mongodb://localhost:27017/clean';
+const localMongoURI = 'mongodb+srv://geetika:Geetika%402005@cluster0.xf42d.mongodb.net/myDatabase?retryWrites=true&w=majority';
 mongoose.connect(localMongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB on local machine'))
     .catch((err) => console.error('MongoDB connection error:', err));
